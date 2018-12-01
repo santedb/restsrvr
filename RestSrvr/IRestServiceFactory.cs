2dd351b34a40a759a1113e8e99e8f65f43af9796
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-01 17:28:58 +0630

    Refactored some more services out of the core

---
+++ RestSrvr/IRestServiceFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSrvr
+{
+    /// <summary>
+    /// Represents a class which can be used to create new services
+    /// </summary>
+    public interface IRestServiceFactory
+    {
+
+        /// <summary>
+        /// Create the specified REST service (from configuration)
+        /// </summary>
+        /// <param name="serviceType">The service behavior</param>
+        /// <returns>The created rest service</returns>
+        RestService CreateService(Type serviceType);
+
+        /// <summary>
+        /// Get service capabilities
+        /// </summary>
+        int GetServiceCapabilities(RestService service);
+    }
+}
