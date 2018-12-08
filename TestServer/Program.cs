8f87c870bc7b0253c941084e1b6a1313608099d9
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-08 09:28:56 +0700

    Minor update to server

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -14,15 +14,12 @@ namespace TestServer
         {
 
             var rservice = new RestService(typeof(SampleBehavior));
-            rservice.AddServiceEndpoint(new Uri("http://0.0.0.0:8080/fhir"), typeof(ISampleContract), new RestHttpBinding());
+            rservice.AddServiceEndpoint(new Uri("http://127.0.0.1:9200/fhir"), typeof(ISampleContract), new RestHttpBinding());
+            rservice.AddServiceEndpoint(new Uri("http://127.0.0.1:9200/"), typeof(ISampleContract), new RestHttpBinding());
             foreach (var spe in rservice.Endpoints)
                 spe.AddEndpointBehavior(new EndpointConsoleLogBehavior());
-
-            var oservice = new RestService(typeof(SampleBehavior));
-            oservice.AddServiceEndpoint(new Uri("http://0.0.0.0:8080/auth"), typeof(ISampleContract), new RestHttpBinding());
-            oservice.AddServiceEndpoint(new Uri("http://0.0.0.0:8080/hdsi"), typeof(ISampleContract), new RestHttpBinding());
             rservice.Start();
-            oservice.Start();
+            //oservice.Start();
             Console.ReadKey();
 
         }
