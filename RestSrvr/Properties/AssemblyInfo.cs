ba4735afa95e414fe2892754dffdfd2df909d5a9
Justin Fyfe <justin@fyfesoftware.ca>
2019-04-12 16:54:31 -0400

    Release of version 1.41

--- RestSrvr/Properties/AssemblyInfo.cs
+++ RestSrvr/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.40.0*")]
-[assembly: AssemblyVersion("1.40.0.*")]
-[assembly: AssemblyFileVersion("1.40.0.0")]
+// [assembly: AssemblyVersion("1.41.0*")]
+[assembly: AssemblyVersion("1.41.0.*")]
+[assembly: AssemblyFileVersion("1.41.0.0")]
 [assembly: AssemblyInformationalVersion("Halifax")]
