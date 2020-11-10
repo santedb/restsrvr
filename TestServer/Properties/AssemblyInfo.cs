afd03244be6ef620ce88a622c197ea16525dc5de
Justin Fyfe <justin@fyfesoftware.ca>
2020-11-10 11:15:46 -0500

    Updated version codes

--- TestServer/Properties/AssemblyInfo.cs
+++ TestServer/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.45*")]
-[assembly: AssemblyVersion("2.0.45.0")]
-[assembly: AssemblyFileVersion("2.0.45.0")]
+// [assembly: AssemblyVersion("2.0.47*")]
+[assembly: AssemblyVersion("2.0.47.0")]
+[assembly: AssemblyFileVersion("2.0.47.0")]
