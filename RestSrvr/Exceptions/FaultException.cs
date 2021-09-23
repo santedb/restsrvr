082fbdd6281d84450accf2629f1c42cf41e73d92
Justin Fyfe <justin@fyfesoftware.ca>
2021-09-23 14:30:24 -0400

    Added result data pass to ctor on fault exception

--- RestSrvr/Exceptions/FaultException.cs
+++ RestSrvr/Exceptions/FaultException.cs
@@ -2,22 +2,23 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using System;
 
 namespace RestSrvr.Exceptions
@@ -27,7 +28,6 @@ namespace RestSrvr.Exceptions
     /// </summary>
     public class FaultException : Exception
     {
-
         /// <summary>
         /// Creates a new fault
         /// </summary>
@@ -38,8 +38,12 @@ namespace RestSrvr.Exceptions
         /// <summary>
         /// Creates a new fault
         /// </summary>
-        public FaultException(Int32 statusCode, String message) : this(statusCode, message, null)
+        public FaultException(Int32 statusCode, String message, Object resultData = null) : this(statusCode, message, null)
         {
+            if (resultData != null)
+            {
+                this.Data.Add("result", resultData);
+            }
         }
 
         /// <summary>
@@ -49,19 +53,18 @@ namespace RestSrvr.Exceptions
         {
             this.StatusCode = statusCode;
         }
+
         /// <summary>
         /// Status code to supply
         /// </summary>
         public Int32 StatusCode { get; set; }
-
     }
+
     /// <summary>
     /// Represents a fault with a type of response body to be sent
     /// </summary>
     public class FaultException<TBody> : FaultException
     {
-
-
         /// <summary>
         /// The body object of the fault
         /// </summary>
@@ -88,7 +91,5 @@ namespace RestSrvr.Exceptions
         {
             this.Body = data;
         }
-
-
     }
 }
