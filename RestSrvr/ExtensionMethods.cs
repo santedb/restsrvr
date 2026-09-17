2fb9fb408d1869478cd2fd1b121b10b27fb2e133
Justin Fyfe <justin@fyfesoftware.ca>
2026-09-17 15:36:18 -0400

    Updated to fix extract preferred mime type for Mono

--- RestSrvr/ExtensionMethods.cs
+++ RestSrvr/ExtensionMethods.cs
@@ -20,8 +20,11 @@ namespace RestSrvr
         /// <param name="me">The http request</param>
         /// <param name="supportedMediaTypes">The supported media types</param>
         /// <returns></returns>
-        public static ContentType GetMostPreferredResponseContentType(this HttpListenerRequest me, params string[] supportedMediaTypes) => me.AcceptTypes?
-                .Select(x => new System.Net.Mime.ContentType(x))
+        public static ContentType GetMostPreferredResponseContentType(this HttpListenerRequest me, params string[] supportedMediaTypes) => 
+            me.AcceptTypes?
+                .SelectMany(o=>o.Split(',')) // HACK: For Mono
+                .Where(o => !String.IsNullOrEmpty(o)) // HACK: For Mono
+                .Select(x => new System.Net.Mime.ContentType(x.Trim()))
                 .Where(x=> supportedMediaTypes?.Length == 0 || supportedMediaTypes.Contains(x.MediaType))
                 .OrderByDescending(x => float.TryParse(x.Parameters["q"], out var q) ? q : 1.0f)
                 .FirstOrDefault() ??
